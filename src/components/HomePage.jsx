757283c0f6a5b1fadff449ab97a480ea435a3bab
Sahitya77 <sahityanijhawan@gmail.com>
2022-06-28 02:02:18 +0530

    homepage edited

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import millify from 'millify'
-import { Typography,Row,Col,Statistic } from 'antd'
-import { Link } from 'react-router-dom'
-const {Title} = Typography;
-const HomePage = () => {
-  return (
-    <div>
-        <Title level={2} className="heading">
-            Global Crypto Stats
-        </Title>
-        <Row>
-            <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
-            <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
-            <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
-            <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
-            <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
-            
-        </Row>
-    </div>
-  )
-}
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
 
-export default HomePage
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+import Loader from './Loader';
+
+const { Title } = Typography;
+
+const HomePage = () => {
+  const { data, isLoading } = useGetCryptosQuery();
+  const globalData=data?.data?.stats;
+
+  if (isLoading) return <Loader/>;
+
+  return (
+    <>
+      <Title level={2} className="heading">Global Crypto Stats</Title>
+      <Row gutter={[32, 32]}>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalData.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value="56" /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value="56" /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value="56"/></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value="56"/></Col>
+        <Col span={12}><Statistic title="Total Markets" value="56" /></Col>
+      </Row>
+
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': "coinranking1.p.rapidapi.com",
+  'x-rapidapi-key': "aced111a56mshfb6d37960178185p11927bjsn0731e5eb3fb1",
+
+};
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+const baseUrl='https://coinranking1.p.rapidapi.com'
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl}),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query:() => createRequest('/coins'),
+    })
+  })
+});
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
+
+// const cryptoApiHeaders={
+//     'X-RapidAPI-Key': 'aced111a56mshfb6d37960178185p11927bjsn0731e5eb3fb1',
+//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+// }
+
