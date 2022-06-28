d67e6e9b9e3aea0f6c2817dac24a62d2e66ce138
Sahitya77 <sahityanijhawan@gmail.com>
2022-06-29 02:49:27 +0530

    main data fetching

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -11,7 +11,7 @@ const { Title } = Typography;
 
 const HomePage = () => {
   const { data, isLoading } = useGetCryptosQuery();
-  const globalData=data?.data?.stats;
+  const globalStats=data?.data?.stats;
 
   if (isLoading) return <Loader/>;
 
@@ -19,12 +19,12 @@ const HomePage = () => {
     <>
       <Title level={2} className="heading">Global Crypto Stats</Title>
       <Row gutter={[32, 32]}>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalData.total} /></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value="56" /></Col>
-        <Col span={12}><Statistic title="Total Market Cap:" value="56" /></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value="56"/></Col>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value="56"/></Col>
-        <Col span={12}><Statistic title="Total Markets" value="56" /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
 
     </>
